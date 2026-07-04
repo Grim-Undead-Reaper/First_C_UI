ef28d68ff32c963ee843afce2bc32b5b25f28842
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-07-04 01:35:49 -0300

    Criando uma função para limpar o histórico de comandos usados, e uma função para apagar um item especifico do histórico, essa está incompleta ainda

--- History.c
+++ History.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include "History.h"
 
-void AddNewCommandToHistory(int* count, char commandList[100][255], char* currentCommand){
+void AddNewCommandToHistory(int* count, char commandList[maxcharsize][maxcommandstorage], char* currentCommand){
     strcpy(commandList[*count], currentCommand);
     *count = *count + 1;
 }
 
-void GetHistory(int size, char commandList[100][255]){
+void GetHistory(int size, char commandList[maxcharsize][maxcommandstorage]){
     printf("ID | Command name\n");
     for (int i = 0; i < size; i++){
         printf("%d | %s\n", i, commandList[i]);
     }
+}
+
+void EraseAllHistory(int* size, char commmandList[maxcharsize][maxcommandstorage]){
+  int size_value = *size;
+
+  for (int i = 0; i < size_value; i++){
+    strcpy(commmandList[i], "");
+  }
+
+  *size = 0;
+}
+
+int EraseHistoryItem(int size, char commmandList[maxcharsize][maxcommandstorage]){
+  int cmdIndex = 0;
+  printf("\nProvide an index to delete: ");
+  scanf("%d", &cmdIndex);
+
+  if (cmdIndex > size){
+    printf("\nOutOfBoundIndex: The provided index '%d' is greater than the number of commands present '%d'\n", cmdIndex, size);
+    return -1;
+  }
+
+  strcpy(commmandList[cmdIndex], "");
+
+  return 0;
 }
--- History.h
+++ History.h
@@ -1,7 +1,12 @@
 #ifndef HISTORY_H
 #define HISTORY_H
 
-void AddNewCommandToHistory(int* count, char commandList[100][255], char* currentCommand);
-void GetHistory(int size, char commandList[100][255]);
+#define maxcharsize 100
+#define maxcommandstorage 255
+
+void AddNewCommandToHistory(int* count, char commandList[maxcharsize][maxcommandstorage], char* currentCommand);
+void GetHistory(int size, char commandList[maxcharsize][maxcommandstorage]);
+void EraseAllHistory(int* size, char commmandList[maxcharsize][maxcommandstorage]);
+int EraseHistoryItem(int size, char commandList[maxcharsize][maxcommandstorage]);
 
 #endif
