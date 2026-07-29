54e5e0c92d501f4b933bcb80a250e544a27d3f1d
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-07-29 09:20:34 -0300

    Pequenas alterações.

--- Commands.c
+++ Commands.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Commands.h"
-#include "History.h"
-#include "User.h"
+#include "include/Commands.h"
+#include "include/History.h"
+#include "include/User.h"
 
-static const char version[] = "v0.0.10";
+static const char version[] = "v0.0.11";
 
 Command commands[] = {
     {"help", "Show all commands.", HELP},
     {"setuser", "Set a new user name.", SETUSER},
     {"getuser", "Get Current User.", GETUSER},
-    {"clear", "Clear console.", CLEARCONSOLE},
+    {"cls", "Clear console.", CLEARCONSOLE},
     {"cmdh", "Get all commands used.", GETHISTORY},
     {"killh", "Clear the command history", CLEARTERMINAL},
     {"killhi", "Erase a specific item from command history", CLEARTERMINALITEM},
@@ -23,6 +23,7 @@ Command commands[] = {
 const int COMMANDSIZE = (sizeof(commands) / sizeof(commands[0]));
 
 enum Functions InputHandler(char* InputArray, int* count, char commandList[100][255]){
+    printf(">>> ");
     scanf("%254s", InputArray);
     printf("\n");
     AddNewCommandToHistory(count, commandList, InputArray);
@@ -47,5 +48,5 @@ void ClearConsole(void){
 }
 
 void CurrentVersion(void){
-    printf("\n[Author]: {Grim-Undead-Reaper}\n[Software]: {First_C_UI}\n[Version]: {%s}\n", version);
+    printf("\n[Author]: {Grim-Undead-Reaper}\n[Software]: {First_C_UI}\n[Version]: {%s}\n\n", version);
 }
